d6e59d890029d3dad58be24b7fead4eb3531a36c
Vladimir Fomene <vladimirfomene@gmail.com>
2018-11-12 09:19:42 +0100

    feed mongodb database with dummy values

--- src/main/java/com/example/movieapi/MovieApiApplication.java
+++ src/main/java/com/example/movieapi/MovieApiApplication.java
@@ -1,12 +1,43 @@
 package com.example.movieapi;
 
+import com.example.movieapi.model.Movie;
+import com.example.movieapi.model.repository.MovieRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class MovieApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApiApplication.class, args);
+	}
+
+
+	@Bean
+	CommandLineRunner init(ReactiveMongoOperations operations, MovieRepository movieRepository) {
+		return args -> {
+			Flux<Movie> productFlux = Flux.just(
+					new Movie(null, "Avenger: Infinity Wars", "Action", LocalDateTime.now()),
+					new Movie(null, "Gladiator", "Drama/Action", LocalDateTime.now()),
+					new Movie(null, "Black Panther", "Action", LocalDateTime.now()))
+					.flatMap(movieRepository::save);
+
+			productFlux
+					.thenMany(movieRepository.findAll())
+					.subscribe(System.out::println);
+
+            /*operations.collectionExists(Product.class)
+                    .flatMap(exists -> exists ? operations.dropCollection(Product.class) : Mono.just(exists))
+					.thenMany(v -> operations.createCollection(Product.class))
+					.thenMany(productFlux)
+					.thenMany(repository.findAll())
+					.subscribe(System.out::println);*/
+		};
 	}
 }
